2fea3ee3242d206d32aa059cc5441b129b7c844c
AmanahLW <71219542+AmanahLW@users.noreply.github.com>
2022-08-26 01:21:14 +0100

    Add files via upload

---
+++ run_snakemake.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#$ -cwd
+#$ -pe smp 1
+#$ -l h_rt=240:0:0
+#$ -l h_vmem=10G
+#$ -o run_snakemake.o
+#$ -e run_snakemake.e
+
+module purge
+module load Snakemake/7.7.0
+module load pandas/1.4.2
+
+snakemake --use-envmodules --profile gridengine  --snakefile Snakefile --keep-going 
