03fc7072df0f1d6d0c526e97ba71fe5f222debe4
AmanahLW <71219542+AmanahLW@users.noreply.github.com>
2022-08-26 01:51:32 +0100

    Update retaingenotypes.py

--- parsing/retaingenotypes.py
+++ parsing/retaingenotypes.py
@@ -2,33 +2,29 @@ import pandas as pd
 import sys
 import re
 import numpy as np
-#allsamplesbcftoolsfilteredqual30nogvcfgtchr$n.txt
-#allsamplesfilteredqual30nogvcfnonomssingvarqualnoheader.txt
-filename = sys.argv[1]  
+
+filename = sys.argv[1]   # accept 2nd arg in command line
 def retaingenotype(gtubsetfile):
   records=[]
   chrlist=[]
-  pattern= re.compile("gvcfgtchr([0-9]*|[a-z]*).txt")
-  chrnum=pattern.search(gtubsetfile).group(1)
+  pattern= re.compile("gvcfgtchr([0-9]*|[a-z]*).txt") # find file pattern 
+  chrnum=pattern.search(gtubsetfile).group(1) # accept chromosomal vcf with genotype subset and ./. noted as 0 
   genotypedf = pd.read_csv(gtubsetfile, header=0, sep="\t")
   spnames=list(genotypedf.columns)
-  #genotypedf = genotypedf.apply(pd.to_numeric, errors='coerce')
-  #sampledflist=list(sampledf)
-  gtdf=pd.DataFrame(columns=spnames)
-  #print(gtdf)
+  
+  
   print(genotypedf)
   print(spnames)
-  #for i in spnames[1:]:
-  #genotypedf.apply(pd.to_numeric(error="ignore"))
+  
   for i in spnames[1:]:
-	  genotypedf[i] = np.where(genotypedf[i].str.match('[0-9]+/[1-9]+') , "1", genotypedf[i])
-	  genotypedf[i] = np.where(genotypedf[i].str.match('0/0'), "0" , genotypedf[i])  
-	  genotypedf[i] = np.where(genotypedf[i].str.match('[0-9]+/[0-9]+') , "1", genotypedf[i])
+	  genotypedf[i] = np.where(genotypedf[i].str.match('[0-9]+/[1-9]+') , "1", genotypedf[i]) # match all gt except for 0/10 ad 0/0
+	  genotypedf[i] = np.where(genotypedf[i].str.match('0/0'), "0" , genotypedf[i])  # match 0/0
+	  genotypedf[i] = np.where(genotypedf[i].str.match('[0-9]+/[0-9]+') , "1", genotypedf[i]) # match geotypes [0-9]+/10
 
   
-  genotypedf[spnames[1:]] = genotypedf[spnames[1:]].astype('int')
+  genotypedf[spnames[1:]] = genotypedf[spnames[1:]].astype('int') # connvert to int from seocndn column
   print(genotypedf)
-  genotypedf['SUM']=genotypedf.iloc[:,1:].sum(axis=1)
+  genotypedf['SUM']=genotypedf.iloc[:,1:].sum(axis=1) # sum 1 in each row and add sum 
   subsetgt=genotypedf[["CHROM", "SUM"]]
   
   subsetgt.to_csv("../filtering/bcftoolstest/allsamplesfilteredqual30chr"+ chrnum +"snpfrq3.txt", sep = "\t", header=True , index=False)
