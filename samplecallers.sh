0d412c18ea4f06301ec1c69ada6ebc2ad96583cd
AmanahLW <71219542+AmanahLW@users.noreply.github.com>
2022-08-26 01:32:24 +0100

    Update samplecallers.sh

--- samplecallers.sh
+++ samplecallers.sh
@@ -1,4 +1,4 @@
-#find */ -name strelka.S1.vcf.gz | sed 'p;s/.gz/ /g' | xargs -n2 mv
+find */ -name strelka.S1.vcf.gz | sed 'p;s/.gz/ /g' | xargs -n2 mv
 find */ | grep _bcftools.g.vcf.gz$ | sed -e 's/_bcftools.g.vcf.gz$//' > samples-bcftools.tsv
 find */ | grep strelka.S1.vcf$ | sed -e 's/strelka.S1.vcf$//' > samples-strelka.tsv 
 find */ | grep gatk.g.vcf.gz$ | sed -e 's/gatk.g.vcf.gz$//' > samples-gatk.tsv
