4c41251c26db86bd1002529cd3f363962b029f08
AmanahLW <71219542+AmanahLW@users.noreply.github.com>
2022-08-26 01:35:31 +0100

    Update retainqualityanddp.py

--- parsing/retainqualityanddp.py
+++ parsing/retainqualityanddp.py
@@ -6,23 +6,23 @@ import re
 #bcf= "/data/scratch/bt211065/20220505_Researchproject/variant-calling-2022-main/snakemake/test/filtering/bcftoolstest/allsamplesfilteredqual30nogvcfv3cleanednoheader.vcf"
 bcf= "/data/scratch/bt211065/20220505_Researchproject/variant-calling-2022-main/snakemake/test/filtering/bcftoolstest/allsamplesfilteredqual30nogvcfnonomssingvarqualnoheader.txt"
 
-with open(bcf, "r") as fn:
+with open(bcf, "r") as fn: # accept whole vcf
   records=[]
   pattern= re.compile("DP=([0-9]*);")
   #dp=pattern.search(filename).group(1)
   for i in fn:
     if "CHROM" in i:
       header=i
-      header=header.split("\t")
+      header=header.split("\t") # get ref and alt
     else:
       i=i.split("\t")	
       qual=i[5]
       info=i[7]
-      info=pattern.search(info).group(1)
-      rec= qual.split() + info.split()
+      info=pattern.search(info).group(1) # serch for dp number in the info field 
+      rec= qual.split() + info.split() # append depth to qual 
       records.append(rec)
   title=header[5].split()
-  title.extend(header[7].split())
+  title.extend(header[7].split()) # output as qual and dp df
   df=pd.DataFrame(records, columns=title)
   
 df.to_csv("/data/scratch/bt211065/20220505_Researchproject/variant-calling-2022-main/snakemake/test/filtering/bcftoolstest/allsamplesfilteredqual30qualdpnomissqualvar.txt", 
