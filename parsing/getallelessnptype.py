48975bc7e7f4e45673e277e5a61284e143e94e94
AmanahLW <71219542+AmanahLW@users.noreply.github.com>
2022-08-26 01:07:34 +0100

    Update getallelessnptype.py

--- parsing/getallelessnptype.py
+++ parsing/getallelessnptype.py
@@ -7,25 +7,26 @@ import pandas as pd
 bcf="/data/scratch/bt211065/20220505_Researchproject/variant-calling-2022-main/snakemake/test/filtering/bcftoolstest/allsamplesfilteredqual30nogvcfnonomssingvarqualnoheader.txt"
 
 def retain_alleles(filename):
-  with open(filename, "r") as fn:
-    record=[]
-    for i in fn:
-      if "CHROM" not in i:
-        type_alleles=i.split("\t")[3:5]
-        allelestr="".join(type_alleles)
-        record.append(type_alleles)  
-    df=pd.DataFrame(record,columns=["REF","ALT"])
-    bibool = (df['REF'].str.len() == 1) & (df['ALT'].str.len() == 1)
-    bi= df.loc[bibool]
-    numbi=len(bi.index)
+#  with open(filename, "r") as fn:
+#    record=[]
+#    for i in fn:
+#      if "CHROM" not in i:
+#        type_alleles=i.split("\t")[3:5]
+#        allelestr="".join(type_alleles)
+#        record.append(type_alleles)  
+    df=pd.DataFrame(record,columns=["REF","ALT"]) # get ref and alt columns
+    bibool = (df['REF'].str.len() == 1) & (df['ALT'].str.len() == 1) # get length of charcter to equal 1 for ref and alt
+    bi= df.loc[bibool] # get subset 
+    numbi=len(bi.index) # get number of rows
     tribool = (df['REF'].str.len() == 1) & (df['ALT'].str.len() == 3) & df["ALT"].str.contains('[ATCG],[ATCG]', regex=True)
+    # get length of ref as one and alt as 3 for letter comman and letter characters + check for multi alleles
     tri= df.loc[tribool] 
     numtri=len(tri.index) 
     quadbool = (df['REF'].str.len() == 1) & (df['ALT'].str.len() == 5) & df["ALT"].str.contains('[ATCG],[ATCG],[ATCG]', regex=True)
     quad=df.loc[quadbool]
     numquad=len(quad.index)
-    
-    count_types=[numbi,numtri,numquad]
+    # make df 
+    count_types=[numbi,numtri,numquad] 
     type_names=["Biallelic", "Triallelic", "Quadrallelic"]
     
     snp= {"SNP type":  type_names,
@@ -38,13 +39,3 @@ def retain_alleles(filename):
     
 test=retain_alleles(bcf)
 print(test)
-	#print(records)
-	#alleles=header[3:5]
-
-	#samples=header[9:]
-	#last_sample=samples[-1][:-1]
-	#samples[-1]=last_sample
-	#alleles.extend(samples)
-
-	#df=pd.DataFrame(records, columns=alleles)
-	#df.to_csv("allsamplesfilteredqual80chr1noheaderrefaltgt.txt", sep = "\t", header=True , index=False )
