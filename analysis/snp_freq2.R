18fafe776d2e45e04518d1690bfb6c5712f739e1
AmanahLW <71219542+AmanahLW@users.noreply.github.com>
2022-08-26 02:02:48 +0100

    Add files via upload

--- analysis/snp_freq2.R
+++ analysis/snp_freq2.R
@@ -5,32 +5,42 @@ library("scales")
 library("ggplot2")
 .libPaths()
 #snptypecount<-"/data/scratch/bt211065/20220505_Researchproject/variant-calling-2022-main/snakemake/test/filtering/gatktest/allsampleschrfilteredqual80snpfreq.txt"
-snptypecount<-"/data/scratch/bt211065/20220505_Researchproject/variant-calling-2022-main/snakemake/test/filtering/bcftoolstest/allsamplesfilteredqual30snpfreq.txt"
+#snptypecount<-"/data/scratch/bt211065/20220505_Researchproject/variant-calling-2022-main/snakemake/test/filtering/bcftoolstest/allsamplesfilteredqual30snpfreq.txt"
 #snptypecount<-"/data/scratch/bt211065/20220505_Researchproject/variant-calling-2022-main/snakemake/test/filtering/bcftoolstest/allsamplesfilteredqual80snpfreq.txt"
-
-
-#snpcountdf<-fread(snptypecount,sep="\t",nThread=10,header=TRUE)
+#snptypecount<-"allsamplesfilteredqual30snpfreqv3.txt"
+options(scipen = 999)
+#snptypecount="allsampleschrfilteredqual80snpfreq.txt"
+snptypecount="allsamplesfilteredqual30snpfreqv5.txt"
 plot_frq<-function(snpfrqdf){
-  df<-read.delim(snpfrqdf, sep =  "\t")
+  df<-read.delim(snpfrqdf, sep =  "\t" ,header = TRUE)
   moddf<-df[!(df$SUM=="0"),]
+ # png("snpfrqgatk.png" , height = 1000, width=1000)
   figure<- ggplot(moddf, aes(x=SUM))+
-      geom_histogram(binwidth = 1, fill="#84FFFF", color="#84FFFF")+
+      geom_histogram(binwidth = 1, fill="#84FFFF", color="#84FFFF")+ 
       labs(x="Number of samples ", y = "Number of variants" , title= "BCFTools") +
       theme_classic() +
-      scale_y_continuous(labels = comma) +
       theme(plot.title=element_text(hjust = 0, size = 20, face = "bold"), 
           text=element_text(size=20),
-          axis.title=element_text(size=20)) +
-      xlim(1, 53) 
-    
-  print(figure)
-  #freq<-as.data.frame.matrix(table(moddf$SUM))
-  #print(sd(freq[2]))
-  return(table(moddf$SUM))
-  #df<-plot_frq(snptypecount)
-  #fig2<-plot_frq(f.name)
+          axis.title=element_text(size=20))
+      xlim(1, 52) 
+ # dev.off()  
+  #print(figure)
+  #print(table(df$SUM))
+  
+  snp<-hist(moddf$SUM, breaks=52)
+  hist(moddf$SUM, breaks=52 , xlab = "Number of samples" , 
+  ylab="Number of variants" , main=NULL , col = "lightblue" , lty="blank" , ylim = c(0,4000000) , xlim = c(1,52))
+  title("BCFTools", adj=0)
+  
+  nonlogcount<-snp$counts
+  print(nonlogcount)
+  print(sd(nonlogcount))
+  print(mean(nonlogcount))
+  return(sd(as.vector(table(df$SUM))))
+  
+  
     
 }
-df<-plot_frq(snptypecount)
+df5<-plot_frq(snptypecount)
+#1745271356 Jul 13 21:36 allsamplesfilteredqual30nogvcf.vcf.gz
 
-dev.off()
---
+++ analysis/thresholds.R
@@ -0,0 +1,89 @@
+install.packages("grid")
+
+library("ggplot2")
+library("scales")
+library("cowplot")
+library("gtable")
+library("grid")
+memory.limit(size=50000000000000000)
+bcf<-"allsamplesbcffilteredqual30qualdpnomissqualvar.txt"
+gatk<-"allsamplesgatkfilteredqual80v2qualdp.txt"
+strelka<-"allsamplesstrelkafilteredqual30qualanddp.txt"
+#strelka<-"/data/scratch/bt211065/20220505_Researchproject/variant-calling-2022-main/snakemake/test/filtering/strelkatest/allsamplesfilteredqual30qualanddp.txt"
+#bcf<-"/data/scratch/bt211065/20220505_Researchproject/variant-calling-2022-main/snakemake/test/filtering/bcftoolstest/allsamplesbcffilteredqual30qualdpnomissingqualscore.txt"
+#bcf<- "/data/scratch/bt211065/20220505_Researchproject/variant-calling-2022-main/snakemake/test/filtering/bcftoolstest/allsamplesfilteredqual30qualdpnomissqualvar.txt"
+#gatk<-"/data/scratch/bt211065/20220505_Researchproject/variant-calling-2022-main/snakemake/test/filtering/gatktest/allsamplesgatkfilteredqual80v2qualdp.txt"
+############Find threshold for vcf strelka filter############
+qualanddp <- read.delim(strelka, sep = "")
+read_file<-function(filename){
+  qualanddp <- read.delim(filename, sep = "\t") # change to non-whitespace for sep in strelka
+  colnames(qualanddp)<-c("QUAL", "DP")
+  qualanddp$QUAL<-as.numeric(as.character(qualanddp$QUAL))
+  qualanddp$DP<-as.numeric(as.character(qualanddp$DP))
+  return(qualanddp)
+
+}  
+
+
+stdf<-read_file(strelka)
+qualst<-plotqual(stdf)
+dpst<-plotdp(stdf)
+
+bcfdf<-read_file(bcf)
+qualbcf<-plotqual(bcfdf)
+dpbcf<-plotdp(bcfdf)
+
+
+
+gatkdf<-read_file(gatk)
+qualgatk<-plotqual(gatkdf)
+dpgatk<-plotdp(gatkdf)
+
+jpeg("density plot.png", width = 1000, height = 1200)
+plots<-plot_grid(qualst,dpst,qualbcf,dpbcf,qualgatk,dpgatk, 
+         labels=c("a) Strelka2"," ", "b) BCFTools", " ", "c) GATK", " "),
+         ncol=2, nrow=3 , align= "hv", label_size = 20 ,hjust=-0.7 , vjust= 0.2, rel_heights = c(10, 10)) +
+        theme(plot.margin = unit(c(1,0.5,0.5,0.5), "cm"))  
+
+removeclips <- ggplot_gtable(ggplot_build(plots))
+removeclips$layout$clip[removeclips$layout$name == "panel"] <- "off"
+
+
+grid.draw(removeclips)
+
+dev.off()
+
+
+#df<-sort_df(gatk)
+plotqual<-function(df){
+  qualdensity<-ggplot(df, aes(QUAL)) + 
+                geom_density(fill= "#84FFFF", color="#84FFFF", na.rm = TRUE) +
+                labs(x="QUAL (log10) ") +
+                theme_classic() + 
+                scale_x_continuous(trans = "log10" , 
+                       labels = function(x) format(x,scientific = FALSE )) +
+                theme( text=element_text(size=20),
+                axis.title=element_text(size=20),
+                plot.margin=margin(t=10))
+ 
+                
+  
+  print(qualdensity)
+  
+}
+0.09
+plotdp<- function(df){
+  dpdensity<-ggplot(df, aes(DP)) +
+              geom_density(fill= "#84FFFF",color="#84FFFF", na.rm = TRUE ,bw=0.09 ) +
+              labs(x="DP (log10) ") +
+              theme(plot.title=element_text(hjust = 0.1),text=element_text(size=0.04)) +
+              theme_classic()  +
+              scale_x_continuous(trans = "log10" ,
+                                 labels = function(x) format(x,scientific = FALSE )) + # include adjust geome density for bcftools
+              theme( text=element_text(size=20),
+              axis.title=element_text(size=20),
+              plot.margin=margin(t=10))  
+  print(dpdensity)
+  
+}
+
