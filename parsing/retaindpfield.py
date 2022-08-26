f84a280e0080111c0f8c723879aba3e2c4c736e9
AmanahLW <71219542+AmanahLW@users.noreply.github.com>
2022-08-26 01:40:38 +0100

    Update retaindpfield.py

--- parsing/retaindpfield.py
+++ parsing/retaindpfield.py
@@ -12,66 +12,17 @@ import seaborn as sb
 
 strelka="/data/scratch/bt211065/20220505_Researchproject/variant-calling-2022-main/snakemake/test/filtering/strelkatest/allsamplesfilteredqual30samples.txt"  
 dpstrelka="/data/scratch/bt211065/20220505_Researchproject/variant-calling-2022-main/snakemake/test/filtering/strelkatest/allsamplesstrelkafilteredqual30dpsumv2.txt" 
-with open(dpstrelka, "a") as writeto:
-  writeto.write("DP\n")
-  with open(strelka, "r") as fn:
-   #f.write("CHR \t POS \t QUAL \t DP" + "\n")
+with open(dpstrelka, "a") as writeto: 
+  writeto.write("DP\n") # write in first line dp to empty file
+  with open(strelka, "r") as fn: # accept only sample fields
    records=[]
    for i,line in enumerate(fn):
-     if i != 0:
+     if i != 0: # start writin to file after first line
       # header=line[:6]
-       #header.extend(line[9:])
-       line = re.split(r'\t|:', line)
-      # chr_pos = line[0][:-3].split("\t")
-     #chr_pos.extend(line[4::9])
-       dp_samples= line[4::10] # 4tn index 
-       dp_samplesint=(list(map(int, dp_samples)))
-       dp=sum(dp_samplesint)
-       writeto.write( str(dp) + "\n")
-      # chr_pos=chr_pos[:6]
-       #chr_pos.extend(dp_samples[1:])
-       #records.append(chr_pos)
-   
-   #print(records)
- #  title=header[:6]  
-  # title.extend(header[9:])
-   #last_el=title[-1].strip()
-   #print(last_el)
-   
-   #name=title[:8]
-   #name.append(last_el)
-   #print(name)
-   #print(title)
-   #print(title)
-   #num= len(dp_samples)
-   #header=["CHR", "POS", "QUAL"]
-   #print(line)
-   #samples=records[0][10:]
-   #header.extend(records[0][10:])
-   #print(records[0])
-   
-   #title=records[0][:5].extend(records[0][9:])
-   
-  # df = pd.DataFrame(records[1:], columns=name)
-   # saving as tsv file
-  # df.to_csv('strelkawithdp50samples.tsv', index= False , sep="\t")
-#   print(df.iloc[:,3:])
-#   df.AvgDP = df.AvgDP.round().astype(float)
-#   #df['AvgDP'] = df['AvgDP'].apply(lambda num:round(num))
-#   print(df['AvgDP'])
-    #f_sort= df.sort_values(by=['AvgDP'],ascending=True)
-#   sb.kdeplot(data=df['AvgDP'])
-    
-#   plot.savefig('AvgDP')
-    #df.to_csv("cleaned_strelkaall.txt",sep = "\t", index=False)
-   
-   
-   
-   
-   
-   
-   
-paste ../filtering/strelkatest/allsamplesfilteredqual30qualscores.txt ../filtering/strelkatest/allsamplesstrelkafilteredqual30dpsumv2.txt | column -s '\t' -t > ../filtering/strelkatest/allsamplesfilteredqual30qualanddp.txt
-[bt211065@frontend8 analysis]$ ^C
-[bt211065@frontend8 analysis]$ tail ../filtering/strelkatest/allsamplesfilteredqual30qualanddp.txt
-
+       #header.extend(line[9:]) 
+       line = re.split(r'\t|:', line) # split into list at tabs between samples and coluns inbetween fields in each sample
+       dp_samples= line[4::10] # start at 4tn index for depth   and jump every 10th element for next depth read 
+       dp_samplesint=(list(map(int, dp_samples)))  
+       dp=sum(dp_samplesint) # find sum of depth for each sample
+       writeto.write( str(dp) + "\n") 
+ 
