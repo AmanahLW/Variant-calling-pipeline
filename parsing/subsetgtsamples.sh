4c2e3912eace02cd1b2aac6035e022eb4e559065
AmanahLW <71219542+AmanahLW@users.noreply.github.com>
2022-09-13 12:13:44 +0100

    Update subsetgtsamples.sh

--- parsing/subsetgtsamples.sh
+++ parsing/subsetgtsamples.sh
@@ -1,9 +1,11 @@
-
+# get genotype column from VCF
 cut -f -1,10-  
 
+# remove text which is not genotype
 perl -spi -e 's/:[^\s]+//g' 
 
+# subsittite missing genotype with 0 
 awk -F "\t" -v OFS="\t" '{ gsub("[.]/[.]", "0"); print $0}' 
 
-
+# get first chrom and header
 grep "CHROM\|^1\s" 
