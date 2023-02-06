9b58adfefa47188871c43ca65f0470ff4dbe0bcf
AmanahLW <71219542+AmanahLW@users.noreply.github.com>
2023-02-06 10:29:58 +0000

    Update retaindpfield.py

--- parsing/retaindpfield.py
+++ parsing/retaindpfield.py
@@ -9,11 +9,11 @@ with open(dpstrelka, "a") as writeto:
   with open(strelka, "r") as fn: # accept only sample fields
    records=[]
    for i,line in enumerate(fn):
-     if i != 0: # start writin to file after first line
+     if i != 0: # start writing to file after first line
       # header=line[:6]
        #header.extend(line[9:]) 
-       line = re.split(r'\t|:', line) # split into list at tabs between samples and coluns inbetween fields in each sample
-       dp_samples= line[4::10] # start at 4tn index for depth   and jump every 10th element for next depth read 
+       line = re.split(r'\t|:', line) # split into list at tabs between samples and colunms inbetween fields in each sample
+       dp_samples= line[4::10] # start at 4th index for depth   and jump every 10th element for next depth read 
        dp_samplesint=(list(map(int, dp_samples)))  
        dp=sum(dp_samplesint) # find sum of depth for each sample
        writeto.write( str(dp) + "\n") 
