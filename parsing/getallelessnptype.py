a11774081501fb2dbbf5bd98d73838ac05c67549
AmanahLW <71219542+AmanahLW@users.noreply.github.com>
2023-02-06 10:25:59 +0000

    Update getallelessnptype.py

--- parsing/getallelessnptype.py
+++ parsing/getallelessnptype.py
@@ -6,19 +6,12 @@ import pandas as pd
 bcf="/data/scratch/bt211065/20220505_Researchproject/variant-calling-2022-main/snakemake/test/filtering/bcftoolstest/allsamplesfilteredqual30nogvcfnonomssingvarqualnoheader.txt"
 
 def retain_alleles(filename):
-#  with open(filename, "r") as fn:
-#    record=[]
-#    for i in fn:
-#      if "CHROM" not in i:
-#        type_alleles=i.split("\t")[3:5]
-#        allelestr="".join(type_alleles)
-#        record.append(type_alleles)  
     df=pd.DataFrame(record,columns=["REF","ALT"]) # get ref and alt columns
     bibool = (df['REF'].str.len() == 1) & (df['ALT'].str.len() == 1) # get length of charcter to equal 1 for ref and alt
     bi= df.loc[bibool] # get subset 
     numbi=len(bi.index) # get number of rows
     tribool = (df['REF'].str.len() == 1) & (df['ALT'].str.len() == 3) & df["ALT"].str.contains('[ATCG],[ATCG]', regex=True)
-    # get length of ref as one and alt as 3 for letter comman and letter characters + check for multi alleles
+    # get length of ref as 1 and alt as 3 letter characters + check for multi alleles
     tri= df.loc[tribool] 
     numtri=len(tri.index) 
     quadbool = (df['REF'].str.len() == 1) & (df['ALT'].str.len() == 5) & df["ALT"].str.contains('[ATCG],[ATCG],[ATCG]', regex=True)
